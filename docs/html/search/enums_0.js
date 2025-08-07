063abaf537aa82229d1efc0899f65bc65b1e5441
Alex <alex.m.ernst14@gmail.com>
2025-08-07 13:09:38 -0600

    Remove generated docs and add to gitignore

--- docs/html/search/enums_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['motor_115',['Motor',['../odometry_8h.html#a3bc45ff9023afee0cc287274e972d7ce',1,'odometry.h']]]
-];
--- docs/html/search/files_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['odometry_2ecpp_63',['odometry.cpp',['../odometry_8cpp.html',1,'']]],
-  ['odometry_2eh_64',['odometry.h',['../odometry_8h.html',1,'']]]
-];
